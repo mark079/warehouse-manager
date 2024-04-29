6914504b90982363e69584ab353a6b534f3e983b
Mark079 <joaopedromaximo@gmail.com>
2024-04-29 18:28:00 -0300

    add required columns

--- src/db/db.js
+++ src/db/db.js
@@ -16,9 +16,9 @@ export async function connectDatabase() {
         // await db.exec("DROP TABLE IF EXISTS transactions;");
         // await db.exec("DROP TABLE IF EXISTS transaction_details;");
 
-        await db.exec("CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, flagN BOOLEAN DEFAULT 1);");
-        await db.exec("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, quantity INTEGER CHECK(quantity >= 0), categoryId INTEGER, flagN BOOLEAN DEFAULT 1, FOREIGN KEY (categoryId) REFERENCES categories(id));");
-        await db.exec("CREATE TABLE IF NOT EXISTS receivers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, flagN BOOLEAN DEFAULT 1);");
+        await db.exec("CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT, flagN BOOLEAN DEFAULT 1);");
+        await db.exec("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT, quantity INTEGER CHECK(quantity >= 0) NOT NULL, categoryId INTEGER, flagN BOOLEAN DEFAULT 1, FOREIGN KEY (categoryId) REFERENCES categories(id));");
+        await db.exec("CREATE TABLE IF NOT EXISTS receivers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT, flagN BOOLEAN DEFAULT 1);");
         await db.exec("CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT CHECK(type IN ('entry', 'exit')), flagN BOOLEAN DEFAULT 1, datetime DATETIME DEFAULT CURRENT_TIMESTAMP);");
         await db.exec("CREATE TABLE IF NOT EXISTS transaction_details (id INTEGER PRIMARY KEY AUTOINCREMENT, productId INTEGER, quantity INTEGER CHECK(quantity > 0), transactionId INTEGER, flagN BOOLEAN DEFAULT 1, FOREIGN KEY (productId) REFERENCES products(id), FOREIGN KEY (transactionId) REFERENCES transactions(id));");
 
