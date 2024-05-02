4b2ab5d51ead8011dde2c6e17594fd0df6fd5878
Mark079 <joaopedromaximo@gmail.com>
2024-05-01 23:40:30 -0300

    move path out of connectDatabase

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ import express from 'express';
 import { connectDatabase } from './src/db/db.js';
 import categoryController from './src/controllers/categoryController.js';
 import receiverController from './src/controllers/receiverController.js';
+import productsController from './src/controllers/productController.js';
 const app = express();
 const PORT = 3000;
 app.use(express.json());
+app.get("/", (req, res) => res.send("Ok!"));
 app.use('/categories', categoryController);
 app.use('/receivers', receiverController);
+app.use('/products', productsController);
 connectDatabase()
     .then(() => {
         // routes
-        app.get("/", (req, res) => res.send("Ok!"));
 
         app.listen(PORT, () => console.log(`Server is running... http://localhost:${PORT}`));
     })
