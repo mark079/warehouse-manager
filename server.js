b016b0d4e6b5cfdf013e25e276050c62ef49399c
Mark079 <joaopedromaximo@gmail.com>
2024-05-06 12:57:58 -0300

    Add controller, route and transaction test

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { connectDatabase } from './src/db/db.js';
 import categoryController from './src/controllers/categoryController.js';
 import receiverController from './src/controllers/receiverController.js';
 import productsController from './src/controllers/productController.js';
+import transictionsController from './src/controllers/transactionController.js';
 const app = express();
 const PORT = 3000;
 app.use(express.json());
@@ -10,6 +11,7 @@ app.get("/", (req, res) => res.send("Ok!"));
 app.use('/categories', categoryController);
 app.use('/receivers', receiverController);
 app.use('/products', productsController);
+app.use('/transictions', transictionsController);
 connectDatabase()
     .then(() => {
         // routes
---
+++ src/controllers/transactionController.js
@@ -0,0 +1,116 @@
+import express from 'express';
+import { connectDatabase } from '../db/db.js';
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const db = await connectDatabase();
+    try {
+        const transactions = await db.all("SELECT * FROM transactions WHERE flagN = 1;");
+        return res.json(transactions);
+    } catch (error) {
+        return res.status(500).json({ error: error });
+    } finally {
+        await db.close();
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const db = await connectDatabase();
+    try {
+        const transaction = await db.get("SELECT * FROM transactions WHERE id = ? AND flagN = 1", [id]);
+        if (!transaction) {
+            return res.status(404).json({ error: "Transação não encontrada!" });
+        }
+        return res.json(transaction);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    } finally {
+        await db.close();
+    }
+});
+
+router.post('/', async (req, res) => {
+    const { type, receiverId, letter, observation } = req.body;
+    const db = await connectDatabase();
+    try {
+        const errors = {};
+        if (!type) {
+            errors.type = "Tipo é um campo obrigatório"
+        } else {
+            if (type !== "exit" && type !== "entry") {
+                errors.type = "Tipo inválido";
+            }
+
+        }
+        if (!receiverId) {
+            errors.receiverId = 'Recebedor é um campo obrigatório';
+        } else {
+            const existingReceiver = await db.get(`SELECT * FROM receivers WHERE id = ${receiverId} AND flagN = 1;`);
+            if (!existingReceiver) {
+                errors.receiverId = 'Recebedor não encontrado';
+            }
+        }
+        if (Object.keys(errors).length > 0) {
+            return res.status(400).json({ errors });
+        }
+        // return res.send(`INSERT INTO transactions (type, receiverId, letter, observation) VALUES ('${type}', ${receiverId}, ${letter || "''"}, ${observation || "''"});`);
+        const result = await db.run(`INSERT INTO transactions (type, receiverId, letter, observation) VALUES ('${type}', ${receiverId}, '${letter || ""}', '${observation || ""}');`);
+        return res.json({ message: 'Transação adicionada com sucesso', ID: result.lastID });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    } finally {
+        await db.close();
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { type, receiverId, letter, observation } = req.body;
+    const db = await connectDatabase();
+    try {
+        const errors = {};
+        if (!type) {
+            errors.type = "Tipo é um campo obrigatório"
+        } else {
+            if (type !== "exit" && type !== "entry") {
+                errors.type = "Tipo inválido";
+            }
+
+        }
+        if (!receiverId) {
+            errors.receiverId = 'Recebedor é um campo obrigatório';
+        } else {
+            const existingReceiver = await db.get(`SELECT * FROM receivers WHERE id = ${receiverId} AND flagN = 1;`);
+            if (!existingReceiver) {
+                errors.receiverId = 'Recebedor não encontrado';
+            }
+        }
+        if (Object.keys(errors).length > 0) {
+            return res.status(400).json({ errors });
+        }
+        await db.run(`UPDATE transactions SET type = '${type}', receiverId = ${receiverId}, letter = ${letter}, observation = '${observation || ""}' WHERE id = ${id} AND flagN = 1;`);
+        return res.json({ message: 'Transação atualizada com sucesso' });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    } finally {
+        await db.close();
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const db = await connectDatabase();
+    try {
+        const existingtransaction = await db.get(`SELECT * FROM transactions WHERE id = ${id} AND flagN = 1;`);
+        if (!existingtransaction) {
+            return res.status(400).json({ error: "Transação não encontrada" });
+        }
+        await db.run(`UPDATE transactions SET flagN = 0 WHERE id = ${id}`);
+        return res.json({ message: 'Transação removida com sucesso' });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+export default router;
