f18429c28c7c5e2863d7dc593612922699c87ec2
Mark079 <joaopedromaximo@gmail.com>
2024-05-06 12:53:18 -0300

    Add missing fields to the transaction table

--- src/db/db.js
+++ src/db/db.js
@@ -19,7 +19,7 @@ export async function connectDatabase() {
         await db.exec("CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT, flagN BOOLEAN DEFAULT 1);");
         await db.exec("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT, quantity INTEGER CHECK(quantity >= 0) NOT NULL, categoryId INTEGER, flagN BOOLEAN DEFAULT 1, FOREIGN KEY (categoryId) REFERENCES categories(id));");
         await db.exec("CREATE TABLE IF NOT EXISTS receivers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT, flagN BOOLEAN DEFAULT 1);");
-        await db.exec("CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT CHECK(type IN ('entry', 'exit')), flagN BOOLEAN DEFAULT 1, datetime DATETIME DEFAULT CURRENT_TIMESTAMP);");
+        await db.exec("CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT CHECK(type IN ('entry', 'exit')), receiverId INTEGER, letter INTEGER, observation TEXT, flagN BOOLEAN DEFAULT 1, datetime DATETIME DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (receiverId) REFERENCES receivers(id));");
         await db.exec("CREATE TABLE IF NOT EXISTS transaction_details (id INTEGER PRIMARY KEY AUTOINCREMENT, productId INTEGER, quantity INTEGER CHECK(quantity > 0), transactionId INTEGER, flagN BOOLEAN DEFAULT 1, FOREIGN KEY (productId) REFERENCES products(id), FOREIGN KEY (transactionId) REFERENCES transactions(id));");
 
         console.log("Conexão com o banco de dados estabelecida");
